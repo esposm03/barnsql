e8f7f3252ddc7f5c312af68553307e30242a3910
esposm03 <36164633+esposm03@users.noreply.github.com>
2022-05-08 12:59:24 +0200

    fix: don't use hashes to identify rows

--- src/table/tab.rs
+++ src/table/tab.rs
@@ -1,14 +1,11 @@
-use std::{
-    collections::hash_map::DefaultHasher,
-    hash::{Hash, Hasher},
-    io::{self, Write},
-};
+use std::io::{self, Write};
 
 use sled::{Db, IVec, Tree};
 
-use crate::util::{display_ivec, serialize_ivec};
-
-use super::{ColumnList, ColumnName, Val};
+use crate::{
+    table::{ColumnList, ColumnName, Val},
+    util::{display_ivec, serialize_ivec},
+};
 
 pub struct Table {
     name: IVec,
@@ -61,8 +58,7 @@ impl Table {
     }
 
     pub fn insert(&self, row: Vec<Val>) -> Result<(), sled::Error> {
-        let mut serialized_row = vec![];
-        let mut pk_hasher = DefaultHasher::new();
+        let mut pk = vec![];
 
         for pk_col in &self.primary_key {
             let col_index = self
@@ -70,31 +66,31 @@ impl Table {
                 .iter()
                 .position(|(col_name, _)| col_name == pk_col)
                 .unwrap();
-            row[col_index].hash(&mut pk_hasher);
+            row[col_index].serialize(&mut pk)?;
         }
 
-        if self.tree.contains_key(pk_hasher.finish().to_le_bytes())? {
+        if self.tree.contains_key(&pk)? {
             panic!("Row already inserted");
         }
+
+        let mut serialized_row = vec![];
 
         for val in &row {
             val.serialize(&mut serialized_row)?;
         }
 
-        self.tree
-            .insert(pk_hasher.finish().to_le_bytes(), serialized_row)?;
+        self.tree.insert(pk, serialized_row)?;
 
         Ok(())
     }
 
     pub fn select(&self, primary_key: Vec<Val>) -> Result<Vec<Val>, sled::Error> {
-        let mut pk_hasher = DefaultHasher::new();
+        let mut pk = vec![];
         for pk_col in primary_key {
-            pk_col.hash(&mut pk_hasher);
+            pk_col.serialize(&mut pk)?;
         }
-        let pk = pk_hasher.finish();
 
-        let row = self.tree.get(pk.to_le_bytes())?.expect("No such row");
+        let row = self.tree.get(pk)?.expect("No such row");
         let mut reader = row.as_ref();
         let mut deserialized = vec![];
 
